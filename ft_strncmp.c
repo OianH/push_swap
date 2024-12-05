00faabcfcf9e5f5fbba8e96a5bc5468d8ee776fc
OianH <158830497+OianH@users.noreply.github.com>
2024-12-05 19:16:27 +0100

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,12 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oiahidal <oiahidal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/07 16:13:21 by oiahidal          #+#    #+#             */
-/*   Updated: 2024/11/07 16:13:22 by oiahidal         ###   ########.fr       */
+/*   Created: 2024/03/04 12:25:30 by oiahidal          #+#    #+#             */
+/*   Updated: 2024/11/21 16:31:09 by oiahidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned int	a;
+	unsigned int	b;
+	size_t			n;
+
+	i = 0;
+	a = ft_strlen(s1);
+	b = ft_strlen(s2);
+	if (a > b)
+		n = a;
+	else
+		n = b;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
--- validate_args.c
+++ validate_args.c
@@ -6,53 +6,131 @@
 /*   By: oiahidal <oiahidal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:50:23 by oiahidal          #+#    #+#             */
-/*   Updated: 2024/11/18 18:54:49 by oiahidal         ###   ########.fr       */
+/*   Updated: 2024/12/05 19:15:30 by oiahidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int validate_str(char *str)
+int	validate_str(char *str)
 {
 	int	i;
 	int	symbol;
-	int sign;
+	int	sign;
 
 	i = 0;
 	symbol = 0;
 	sign = 0;
-
 	while (str[i])
 	{
 		if ((str[i] >= 33 && str[i] <= 47 && str[i] != '-' && str[i] != '+')
-		|| (str[i] >= 58 && str[i] <= 127 && str[i] != '-' && str[i] != '+'))
-			return (0);
+			|| (str[i] >= 58 && str[i] <= 127
+				&& str[i] != '-' && str[i] != '+'))
+			return (1);
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
-int validate_sign(char *str)
+int	validate_sign(char *str)
 {
-	int i;
-	int sign;
+	int	i;
+	int	sign;
 
 	i = 0;
 	sign = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == '-' || str[i] == '+')
 		{
 			i++;
 			if (!(ft_isdigit(str[i])))
-				return (0);
+				return (1);
 		}
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
-int validate_numbers(int **nbrs)
+int	validate_numbers(char **nbrs)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (nbrs[i])
+	{
+		while (nbrs[j])
+		{
+			if (!ft_strcmp(nbrs[i], nbrs[j]))
+				j++;
+			else
+				return (1);
+		}
+		i++;
+		j = i + 1;
+	}
+	return (0);
+}
+
+int	validate_new_args(int argc, char **args)
+{
+	int	i;
+	char	**new_args;
+
+	i = 0;
+	if (argc == 2)
+		new_args = ft_split(args[1])
+	else
+		new_args = args[1];
+	while (new_args[i])
+		i++;
+	if (i == 1)
+		return (1);
+	else
+		return (0);
 	
+}
+
+int	validate_args(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	int		errors;
+	char	**new_args;
+
+	i = 0;
+	errors = 0;
+	if (argc == 2)
+		new_args = ft_split(argv[1]);
+	else if (argc < 2)
+		// error args insuficientes
+	else
+		new_args = argv;
+	if (validate_new_args(new_args))
+		// error args insuficientes
+	while (new_args[i])
+	{
+		while (new_args[j])
+		{
+			if (validate_str(new_args[j]))
+				errors = 1;
+			i++;
+		}
+		while (new_args[j])
+		{
+			if (validate_sign(new_args[j]))
+				errors = 2;
+			i++;
+		}
+		while (new_args[j])
+		{
+			if (validate_numbers(new_args[j]))
+				errors = 3;
+			i++;
+		}
+		j = 0;
+		i++;
+	}
 }
